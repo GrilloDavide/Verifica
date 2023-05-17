ff40686cc4056654e2bfd8a775b4dd5f31883e6d
lai2-01 <lai2-01@itismeucci.com>
2023-05-17 12:16:51 +0200

    inizio verifica

---
+++ src/Turista.java
@@ -0,0 +1,14 @@
+public class Turista extends Thread{
+    
+    private String cognome;
+
+
+    public Turista(String cognome) {
+        this.cognome = cognome;
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+}
